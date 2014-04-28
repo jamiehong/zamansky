617d651debc5c8a119f9ea31089a0586acc04344
jamiehong <jamiehong.stuy@gmail.com>
2014-04-27 20:41:55 -0400

    the node is right…probably

---
+++ 16-BST1/BTS.java
@@ -0,0 +1,14 @@
+public class BST {
+    private Node root;
+
+    public BST() {
+        root = null;
+    }
+
+    public void insert(int i){
+	if (root = null){
+	    root = new Node(i);
+	}
+    }
+
+}
